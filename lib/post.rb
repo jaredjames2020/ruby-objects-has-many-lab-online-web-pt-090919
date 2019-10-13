f0f2c15fe543aef7dd47ac51af2aeb11dd0da902
Jared S <jaredjames2020@gmail.com>
2019-10-13 19:53:45 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -14,7 +14,13 @@ class Post
   end
   
   def author_name
-    self.author.name
+    #self.author.name
+    
+    if self.author.name == nil
+      return nil
+    else 
+      self.author.name
+    end
   end
   
 end
