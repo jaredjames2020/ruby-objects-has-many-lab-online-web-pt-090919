69c5ba3f3bd0e4a7d496b3ce2fa6bf307fb1573d
Jared S <jaredjames2020@gmail.com>
2019-10-13 19:41:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,9 +7,19 @@ class Author
     @posts = []
   end
   
-    
+  def add_post(post)
+  
+  
+  end
 
-
+  def add_post_by_title(post_title)
+    post_title = Post.new(post_title)
+    post_title
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
 
 
 end
